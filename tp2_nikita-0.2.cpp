d93e76ba73bd83eb02f783f2daf45d0a4d5ffd9a
Nikita Fomishyn <niikfm@gmail.com>
2013-07-06 18:50:39 -0300

    Pasando vectores a menu_data

--- tp2_nikita-0.2.cpp
+++ tp2_nikita-0.2.cpp
@@ -6,14 +6,20 @@
 
 bool login_request();
 bool login_manager(char *, char *);
-void menu_data(bool *);
+void menu_data(bool *, int , int *, int *);
 short int menu_items();
+void data_processing();
 
 int main() 
 {
+   int SueldoAnualProfesional[81]; for(int id = 0; id <= 81; id++) { SueldoAnualProfesional[id] = 0; }
+   int SueldoMes[12]; for(int id = 0; id <= 12; id++) { SueldoMes[id] = 0; }
+   int SueldoLegajo[81]; for(int id = 0; id <= 81; id++) { SueldoLegajo[id] = 0; }
+   
    bool data = false;
    int menu_item;
    system("clear"); //system("cls")
+   
    if(login_request()) // nos logeamos
      {	
 	while(menu_item != 5) // repetimos el switch hasta salir del menu
@@ -23,7 +29,7 @@ int main()
 	     if(data == false && menu_item == 0) // opcion 0 usable solo si no introducimos nada
 	       {
 		  system("clear"); //system("cls");
-		  menu_data(&data); // introducimos datos
+		  menu_data(&data, SueldoAnualProfesional, SueldoMes, SueldoLegajo); // introducimos datos
 	       }
 	     else if(data == true) // si ya introducimos datos habilitamos las otras opciones
 	       {
@@ -35,16 +41,20 @@ int main()
 		       break;
 		       
 		     case 1:
-		       system("clear")
+		      system("clear");
+			     data_processing();
 		       break;
 		     case 2:
-		       system("clear")
+		       system("clear");
+                
 		       break;
 		     case 3:
-		       system("clear")
+		       system("clear");
+               
 		       break;
 		     case 4:
-		       system("clear")
+		       system("clear");
+               
 		       break;
 		    }
 	       }
@@ -106,34 +116,59 @@ bool login_manager(char *tmp_login, char *tmp_paswd) {
    }
 }
 
-void menu_data(bool *data) // llenamos los datos
+void menu_data(bool *data, int SueldoAnualProfesional[81], int *SueldoMes, int *SueldoLegajo) // llenamos los datos
 {
    int tmp_legajo;
    int tmp_categoria;
    int tmp_horas;
    int tmp_mes;
    
+   int SueldoAnualProfesional[3] = 100;
+   printf("%d\n", SueldoAnualProfesional[3]);
+
+   int tmp_numero = 0;
+   
+   bool validated = true;
+   
    while(tmp_legajo != 100) 
-     {
-	bool validated = true;
-	system("clear"); //system("cls");
-	printf("Introduce los datos de empleado:");
-	printf("\nLegajo: ");		        scanf("%d", &tmp_legajo); if(tmp_legajo == 100) { break; } else if(tmp_legajo < 0) { validated = false; }
- 	printf("\nCategoria: ");	        scanf("%d", &tmp_categoria); if(tmp_categoria < 0 || tmp_categoria > 6) { validated = false; }
-	printf("\nHoras Extra: ");      	scanf("%d", &tmp_horas) if(tmp_horas < 0) { validated = false; };
-	printf("\nNumero de mes [1 - 12]: ");	scanf("%d", &tmp_mes); if(tmp_mes <= 0 || tmp_mes > 12) { validated = false; }
-     }
+    {
+    	//system("clear"); //system("cls");
+    	printf("\nIntroduce los datos #%d:", tmp_numero);
+    	printf("\nNumero de legajo: ");		        scanf("%d", &tmp_legajo); 
+        if(tmp_legajo == 100) { break; } else if(tmp_legajo < 1) { 
+            validated = false; }
+
+     	printf("\nCategoria perteneciente: ");	        scanf("%d", &tmp_categoria); 
+        if(tmp_categoria < 1 || tmp_categoria > 6) { 
+            validated = false; }
+
+    	printf("\nHoras Extra trabajadas: ");      	scanf("%d", &tmp_horas); 
+        if(tmp_horas < 0) { 
+            validated = false; }
+
+    	printf("\nNumero del mes [1 - 12]: ");	scanf("%d", &tmp_mes); 
+        if(tmp_mes <= 0 || tmp_mes > 12) { 
+            validated = false; }
+
+
+
+        if(validated == true) 
+        {
+            *data = true; // modificamos 'data' a travez de su puntero para poder habilitar el menu al terminar de introducir los datos
+            tmp_numero += 1;
+            getchar();
+            system("clear"); //system("cls");
+        }
+        else 
+        {   
+            system("clear"); //system("cls");
+            printf("Los datos de la introduccion #%d no han sido validados por favor vuelva a introducirlos.", tmp_numero);
+            validated = true;
+            getchar();
+        }
+    }
    
-   if(validated == true)
-     {
-	*data = true; // modificamos 'data' a travez de su puntero para poder habilitar el menu al terminar de introducir los datos
-     }
-   else
-     {
-	printf("Los datos no han sido validados por no respetar el formato");
-     }
-   
-   system("clear"); //system("cls");
+   //system("clear"); //system("cls");
 }
 
 short int menu_items() // menu separado para poder imprimirlo cuando lo necesitemos
@@ -152,4 +187,33 @@ short int menu_items() // menu separado para poder imprimirlo cuando lo necesite
    
    return menu_item;
 } 
-   
+
+void data_processing()
+{
+   char descr[7][64] = 
+     { {"Descr1"}, {"Descr2"}, {"Descr3"}, {"Descr4"}, {"Descr5"}, {"Descr6"} };
+   int salary[7] =
+     { 0, 100, 200, 300, 400, 500, 600 };
+   int extra[7] = 
+     { 0, 10, 20, 30, 40, 50, 60 };
+
+
+    int menu_item =0;
+
+    switch(menu_item) 
+    {
+        case 1:
+        break;
+
+        case 2:
+        break;
+
+        case 3:
+        break;
+
+        case 4:
+        break;
+
+    }
+	
+}
