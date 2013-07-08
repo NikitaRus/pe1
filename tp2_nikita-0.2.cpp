6a74ef36657c47e4022afe7e4d78ae9c78039ae1
Nikita Fomishyn <niikfm@gmail.com>
2013-07-07 22:51:22 -0300

    Implementando el proceso final de los datos

--- tp2_nikita-0.2.cpp
+++ tp2_nikita-0.2.cpp
@@ -4,22 +4,47 @@
 #include <unistd.h> /*no se si esta en windows, buscar y borrar "sleep(2)"*/
 //#include <windows.h>
 
+
+#define LOGIN "Nikita"
+#define PASSWORD "1234"
+
+#define LEGAJO 81
+#define CATEGORIA 7
+#define DESCRIPCION 64
+#define MES 12
+#define EMPLEADOS 80
+
+#define SALIDA 100
+
+
+//
 bool login_request();
-bool login_manager(char *, char *);
-void menu_data(bool *, int *, int *, int *);
+bool login_validator(char *, char *);
+void menu_data(bool *, int *, int *, int *, int *, int *, int *);
 short int menu_items();
-void data_processing();
+void data_processing(int, int *, int *);
+//
 
 int main() 
 {
-   int SueldoAnualProfesional[81] = {0}; 
-   int SueldoMes[12] = {0};
-   int SueldoLegajo[81] = {0};
+   int SueldoAnualProfesional[LEGAJO] = {0}; 
+   int SueldoMes[MES] = {0};
+   int SueldoLegajo[LEGAJO] = {0};
+   int TotalHorasExtra[LEGAJO] = {0};
+
+   char DescripcionCategoria[CATEGORIA][DESCRIPCION] = 
+     { {"Descr1"}, {"Descr2"}, {"Descr3"}, {"Descr4"}, {"Descr5"}, {"Descr6"} };
+
+   int SalarioCategoria[CATEGORIA] =
+     { 1000, 2000, 3000, 4000, 5000, 6000 };
+
+   int CostoHoraExtraCategoria[CATEGORIA] = 
+     { 10, 20, 30, 40, 50, 60 };
    
    bool data = false;
    int menu_item;
+
    system("clear"); //system("cls")
-   
    if(login_request()) // nos logeamos
      {	
 	while(menu_item != 5) // repetimos el switch hasta salir del menu
@@ -29,7 +54,7 @@ int main()
 	     if(data == false && menu_item == 0) // opcion 0 usable solo si no introducimos nada
 	       {
 		  system("clear"); //system("cls");
-		  menu_data(&data, SueldoAnualProfesional, SueldoMes, SueldoLegajo); // introducimos datos
+		  menu_data(&data, SueldoAnualProfesional, SueldoMes, SueldoLegajo, SalarioCategoria, CostoHoraExtraCategoria, TotalHorasExtra); // introducimos datos
 	       }
 	     else if(data == true) // si ya introducimos datos habilitamos las otras opciones
 	       {
@@ -38,14 +63,16 @@ int main()
 		     case 0:
 		       system("clear"); //system("cls");
 		       printf("Solo se puede introducir los datos una vez\n");
+               getchar();
 		       break;
 		       
 		     case 1:
 		      system("clear");
-			     data_processing();
+			   data_processing(menu_item, SueldoAnualProfesional, 0);
 		       break;
 		     case 2:
 		       system("clear");
+               data_processing(menu_item, SueldoMes, 0);
                 
 		       break;
 		     case 3:
@@ -53,7 +80,7 @@ int main()
                
 		       break;
 		     case 4:
-		       system("clear");
+		       data_processing(menu_item, SueldoAnualProfesional, TotalHorasExtra);
                
 		       break;
 		    }
@@ -87,7 +114,7 @@ bool login_request() {
       printf("Username: "); fflush(stdin); gets(tmp_login); // Compilador de Linux tirando warnings de que 'gets' no apto para el uso
       printf("Password: "); fflush(stdin); gets(tmp_paswd);
       
-      if(login_manager(tmp_login, tmp_paswd)) {
+      if(login_validator(tmp_login, tmp_paswd)) {
 	 printf("Logeado\n"); system("clear"); //system("cls")
 	 return true;
 	 break;
@@ -103,39 +130,39 @@ bool login_request() {
    }
 }
 
-bool login_manager(char *tmp_login, char *tmp_paswd) {
-   //Lo correcto seria mudar los usuarios a .ini/.dat/DB y sacarlos de ahi
-   char login[16] = "Nikita";
-   char paswd[32] = "1234";
+bool login_validator(char *tmp_login, char *tmp_paswd) {
+    //Separado del resto de login_request para poder implementar en el futuro la lectura de datos
+    //de una fuente externa o desencriptacion de contrasena como funciones separadas.
+
+    //Lo correcto seria mudar los usuarios a .ini/.dat/DB y leerlos de ahi encriptados
+    char login[16] = LOGIN;
+    char paswd[32] = PASSWORD;
    
-   //!strcmp parte de string.h para comparar arrays de chars de manera que devuelven true o false y se autoverifican
-   if(!strcmp(login, tmp_login) && !strcmp(paswd, tmp_paswd)) {
-      return true; //Logeado
-   } else {
-      return false; //Fallo
-   }
+    //!strcmp parte de string.h para comparar arrays de chars de manera que devuelven true o false y se autoverifican
+    if(!strcmp(login, tmp_login) && !strcmp(paswd, tmp_paswd)) {
+        return true; //Logeado
+    } else {
+        return false; //Fallo
+    }
 }
 
-void menu_data(bool *data, int *SueldoAnualProfesional, int *SueldoMes, int *SueldoLegajo) // llenamos los datos
+void menu_data(bool *data, int *SueldoAnualProfesional, int *SueldoMes, int *SueldoLegajo, int *CostoHoraExtraCategoria, int *SalarioCategoria, int *TotalHorasExtra) // llenamos los datos
 {
    int tmp_legajo;
    int tmp_categoria;
    int tmp_horas;
    int tmp_mes;
-   
-   SueldoAnualProfesional[3] = 100;
-   printf("%d\n", SueldoAnualProfesional[3]);
 
    int tmp_numero = 0;
    
    bool validated = true;
    
-   while(tmp_numero != 80) 
+   while(tmp_numero <= EMPLEADOS) 
     {
     	//system("clear"); //system("cls");
     	printf("\nIntroduce los datos #%d:", tmp_numero);
     	printf("\nNumero de legajo: ");		        scanf("%d", &tmp_legajo); 
-        if(tmp_legajo == 100) { break; } else if(tmp_legajo < 1) { 
+        if(tmp_legajo == SALIDA) { break; } else if(tmp_legajo < 1) { 
             validated = false; }
 
      	printf("\nCategoria perteneciente: ");	        scanf("%d", &tmp_categoria); 
@@ -151,11 +178,25 @@ void menu_data(bool *data, int *SueldoAnualProfesional, int *SueldoMes, int *Sue
             validated = false; }
 
 
-
         if(validated == true) 
         {
+            int pago = SalarioCategoria[tmp_categoria] + (tmp_horas * CostoHoraExtraCategoria[tmp_categoria]);
+
             *data = true; // modificamos 'data' a travez de su puntero para poder habilitar el menu al terminar de introducir los datos
-            tmp_numero += 1;
+
+            tmp_numero += 1; // Aumentamos el numero actual en el orden del input (para tener un debug de cual nos tira error)
+
+            SueldoAnualProfesional[tmp_legajo] += pago; // Definimos el sueldo anual de cada profesional
+            SueldoMes[tmp_mes] += pago; // Definimos el total pagado por mes
+            TotalHorasExtra[tmp_legajo] += tmp_horas;
+
+
+            if(tmp_mes == 7 || tmp_mes == 12) {
+                SueldoAnualProfesional[tmp_legajo] += pago / 2;
+                SueldoMes[tmp_mes] += pago / 2;
+            } // Agregamos aguinaldo a 'pagado por mes' y 'sueldo de cada profesional'
+
+
             getchar();
             system("clear"); //system("cls");
         }
@@ -171,7 +212,7 @@ void menu_data(bool *data, int *SueldoAnualProfesional, int *SueldoMes, int *Sue
    //system("clear"); //system("cls");
 }
 
-short int menu_items() // menu separado para poder imprimirlo cuando lo necesitemos
+short int menu_items() // menu separado para poder imprimirlo cuando lo necesite
 {
    int menu_item;
    
@@ -188,31 +229,77 @@ short int menu_items() // menu separado para poder imprimirlo cuando lo necesite
    return menu_item;
 } 
 
-void data_processing()
+void data_processing(int menu_item, int *Argument1 = 0, int *Argument2 = 0)
 {
-   char descr[7][64] = 
-     { {"Descr1"}, {"Descr2"}, {"Descr3"}, {"Descr4"}, {"Descr5"}, {"Descr6"} };
-   int salary[7] =
-     { 0, 100, 200, 300, 400, 500, 600 };
-   int extra[7] = 
-     { 0, 10, 20, 30, 40, 50, 60 };
-
-
-    int menu_item =0;
-
     switch(menu_item) 
     {
         case 1:
-        break;
-
+            printf("Sueldo anual de cada profesional:\n");
+            for (int i = 0; i <= LEGAJO; i++) {
+                if (Argument1[i] != 0)
+                {
+                    printf("Sueldo: %d$ | Legajo: #%d\n", Argument1[i], i);
+                }
+                
+            } break;
         case 2:
-        break;
-
+            printf("Total pagado por mes:\n");
+            for (int i = 0; i <= MES; i++) {
+                if (Argument1[i] != 0)
+                {
+                    printf("Total: %d$ | Mes: #%d\n", Argument1[i], i);
+                }
+                
+            } break;
         case 3:
-        break;
+            int tmp_avglegajo;
+
+            for (int i = 0; i <= LEGAJO; i++) {
+                if (Argument1[i] != 0)
+                {
+                    
+                }
+                
+            }
+            break;
 
         case 4:
-        break;
+            //falta mucha optimizacion en esta seccion
+            int LegajoOrdenadoSueldo[LEGAJO] = { *Argument1 }; //Creamos un vector igual al original para ordenarlo y sacar los index
+            int LegajoIndexOrdenado[LEGAJO] = { 0 }; //numeros de legajo en el orden correcto para llamar todos los datos por ese orden.
+            int tmp_mem;
+
+            for(int times = 0; times <= LEGAJO - 1; times++) {
+                for (int item = 0; item <= LEGAJO - times - 1; item++) { 
+                    if (LegajoOrdenadoSueldo[item] > LegajoOrdenadoSueldo[item + 1]) 
+                    {
+                        tmp_mem = LegajoOrdenadoSueldo[item];
+                        LegajoOrdenadoSueldo[item] = LegajoOrdenadoSueldo[item + 1];
+                        LegajoOrdenadoSueldo[item + 1] = tmp_mem;
+
+                        tmp_mem = item;
+                        LegajoIndexOrdenado[item] = item + 1;
+                        LegajoIndexOrdenado[item + 1] = tmp_mem;
+                    }
+                }
+            }
+
+            for(int items = 0; items <= LEGAJO; items++)
+            {
+                int item = LegajoIndexOrdenado[item];
+
+                printf("Sueldo: %d\n", Argument1[item]);
+                printf("  Legajo: %d\n", item);
+                printf("  Horas extra: %d\n\n", Argument2[item]);
+            }
+
+            getchar();
+
+
+
+            
+
+            break;
 
     }
 	
